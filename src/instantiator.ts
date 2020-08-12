cbe4b62cb6d8b6c378b9d59668942cbf5177eec5
Jack Sun <syy1125@hotmail.com>
2020-08-12 12:03:53 -0700

    refactor: Keep consistent level of detail

--- src/instantiator.ts
+++ src/instantiator.ts
@@ -35,42 +35,44 @@ export interface ProblemInstance {
   solutionValue: string | number
 }
 
+const getSolution = (
+  template: ProblemTemplate,
+  variables: Record<string, string>
+): Pick<ProblemInstance, 'solutionExpression' | 'solutionValue'> => {
+  switch (template.solutionType) {
+    case 'EVALUATE': {
+      const solutionExpression = interpolate(
+        template.solutionTemplate,
+        variables
+      )
+      return {
+        solutionExpression,
+        solutionValue: evaluate(solutionExpression),
+      }
+    }
+    case 'SIMPLIFY': {
+      const solutionExpression = interpolate(
+        template.solutionTemplate,
+        variables
+      )
+      return {
+        solutionExpression,
+        solutionValue: simplify(solutionExpression).toString(),
+      }
+    }
+  }
+}
+
 export const instantiate = (template: ProblemTemplate): ProblemInstance => {
   const variables = generateVariables(template.variables)
 
   const questionText = interpolate(template.questionTemplate, variables)
 
-  const getSolution = (): Pick<
-    ProblemInstance,
-    'solutionExpression' | 'solutionValue'
-  > => {
-    switch (template.solutionType) {
-      case 'EVALUATE': {
-        const solutionExpression = interpolate(
-          template.solutionTemplate,
-          variables
-        )
-        return {
-          solutionExpression,
-          solutionValue: evaluate(solutionExpression),
-        }
-      }
-      case 'SIMPLIFY': {
-        const solutionExpression = interpolate(
-          template.solutionTemplate,
-          variables
-        )
-        return {
-          solutionExpression,
-          solutionValue: simplify(solutionExpression).toString(),
-        }
-      }
-    }
-  }
+  const solutionData = getSolution(template, variables)
 
   return {
     variables,
     questionText,
-    ...getSolution(),
+    ...solutionData,
   }
 }
