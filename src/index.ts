6718d799cf32bddc26b8cd440f719f878c0be24e
Jack Sun <syy1125@hotmail.com>
2020-08-14 11:19:58 -0700

    chore: Export errors in index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { interpolate } from './interpolation'
 export { VariableTemplate } from './generator'
 export { instantiate, ProblemTemplate, ProblemInstance } from './instantiator'
+export * from './errors'
